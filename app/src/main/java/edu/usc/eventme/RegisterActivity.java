2085c0aa2772d50f4b4a9da1451ac5fb02f10b4d
Mengfei Qi <mengfeiq@usc.edu>
2022-10-22 19:13:30 -0700

    add register page and intent from login to register

--- app/src/main/java/edu/usc/eventme/RegisterActivity.java
+++ app/src/main/java/edu/usc/eventme/RegisterActivity.java
@@ -4,19 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
     }
 
-    public void goToRegister(View view) {
-        Intent intent = new Intent(this, RegisterActivity.class);
-        startActivity(intent);
-    }
 }
