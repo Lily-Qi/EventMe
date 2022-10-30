3bd0bfe2cb7c2db64a5d4de4c4ca48333c82d532
Sihao Yang <89471780+sihaoyan@users.noreply.github.com>
2022-10-29 22:04:18 -0700

    update

--- app/src/main/java/edu/usc/eventme/Event.java
+++ app/src/main/java/edu/usc/eventme/Event.java
@@ -1,11 +1,36 @@
 package edu.usc.eventme;
-
+import  java.util.ArrayList;
 public class Event {
     private String eventTitle;
+    private int id;
+    private String eventType;
+    private String startDate;
+    private String endDate;
+    private String startTime;
+    private String endTime;
+    private int registered;
+    private double cost;
+    private String content;
+    private String location;
+    private String photo;
+    private boolean parking;
+    private ArrayList<String> times;
 
-    public Event() {}
+    public Event() {
+        times=new ArrayList<String>();
+    }
 
     public String getEventTitle() {
         return eventTitle;
     }
+    public int getId(){return id;}
+    public String getEventType(){return eventType;}
+    public ArrayList<String> getTimes(){
+        return times;
+    }
+    public int getRegistered(){ return registered;}
+    public double getCost(){return cost;}
+    public String getContent(){return content;}
+    public String getLocation(){return location;}
+    public  boolean getParking(){return parking;}
 }
--- app/src/main/java/edu/usc/eventme/EventList.java
+++ app/src/main/java/edu/usc/eventme/EventList.java
@@ -1,13 +1,61 @@
 package edu.usc.eventme;
-
+import java.util.Comparator;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class EventList {
-    private List<Event> eventList;
+    private ArrayList<Event> eventList;
+    private String sorting;
+    private String keyWord;
 
-    public EventList() {};
+    public EventList() {
+        eventList=new ArrayList<Event>();
+    };
 
     public List<Event> getEventList() {
         return eventList;
+    }
+    public void formList(String sort, String key){
+
+    }
+    public void sort(String sort){
+        switch (sort) {
+            case "cost":
+                Collections.sort(eventList, new sortByCost());
+        }
+    }
+
+    public void addEvent(Event e){
+        eventList.add(e);
+    }
+
+    public void removeEvent(Event e){
+        eventList.remove(e);
+    }
+
+    public Event getEvent(int n){
+        for(Event e:eventList){
+            if(e.getId()==n){
+                return e;
+            }
+        }
+
+        return null;
+    }
+
+}
+
+class sortByCost implements Comparator<Event>{
+    public int compare(Event a, Event b){
+        if(a.getCost()>b.getCost()){
+            return 1;
+        }
+        else if(a.getCost()<b.getCost()){
+            return -1;
+        }
+        else{
+            return 0;
+        }
     }
 }
